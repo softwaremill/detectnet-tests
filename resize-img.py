6edd15d6fcf51506dc7741aa2400a91a479daf07
softberries <krzysztof.grajek@googlemail.com>
2017-09-27 17:03:11 +0200

    fixed bug when resizing images

--- resize-img.py
+++ resize-img.py
@@ -9,7 +9,8 @@ sourceDirectory='/Users/kris/Downloads/football1'
 destinationDirectory='/Users/kris/Downloads/football2-resized2'
 
 if not os.path.exists(destinationDirectory):
-    os.makedirs(destinationDirectory)
+    os.makedirs(destinationDirectory + '/slices')
+    os.makedirs(destinationDirectory + '/resized')
 
 # Slice images one by one and copy them to the new directory
 for filename in os.listdir(sourceDirectory):
@@ -17,15 +18,15 @@ for filename in os.listdir(sourceDirectory):
         print("Slicing: {0}".format(os.path.join(sourceDirectory, filename)))
         pre, ext = os.path.splitext(filename)
         tiles = image_slicer.slice(os.path.join(sourceDirectory, filename), number_of_slices, save=False)
-        image_slicer.save_tiles(tiles, directory=destinationDirectory, prefix=pre)
+        image_slicer.save_tiles(tiles, directory=destinationDirectory + '/slices', prefix=pre)
 
 
-for filename in os.listdir(destinationDirectory):
+for filename in os.listdir(destinationDirectory + '/slices'):
     if filename.endswith(".png"):
-        print("Resizing: {0}".format(os.path.join(destinationDirectory, filename)))
-        img = Image.open(os.path.join(destinationDirectory, filename))
+        print("Resizing: {0}".format(os.path.join(destinationDirectory + '/slices', filename)))
+        img = Image.open(os.path.join(destinationDirectory + '/slices', filename))
         img = img.resize((new_width, new_height), Image.ANTIALIAS)
-        img.save(os.path.join(destinationDirectory, filename))
+        img.save(os.path.join(destinationDirectory + '/resized', filename))
         continue
     else:
         continue
